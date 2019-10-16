b9ab2a082c144b72817a0bc9c99a3ebbf543c60e
matjanko <mateusz.jankowski@prokonto.pl>
2019-10-17 00:02:36 +0200

    Add reinforcing bar standard diameter

---
+++ src/main/java/com/github/matjanko/engineer/concrete/cover/domain/bar/Diameter.java
@@ -0,0 +1,17 @@
+package com.github.matjanko.engineer.concrete.cover.domain.bar;
+
+public enum Diameter {
+    FI_6,
+    FI_8,
+    FI_10,
+    FI_12,
+    FI_14,
+    FI_16,
+    FI_18,
+    FI_20,
+    FI_22,
+    FI_25,
+    FI_28,
+    FI_32,
+    FI_40
+}
