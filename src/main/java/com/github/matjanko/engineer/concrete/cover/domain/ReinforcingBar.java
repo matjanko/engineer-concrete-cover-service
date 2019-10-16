2a3efe4654d018587d90256e95284c001c368695
matjanko <mateusz.jankowski@prokonto.pl>
2019-10-17 00:28:19 +0200

    Delete diameter enums. Change reinforcing bar implementation

---
+++ src/main/java/com/github/matjanko/engineer/concrete/cover/domain/ReinforcingBar.java
@@ -0,0 +1,13 @@
+package com.github.matjanko.engineer.concrete.cover.domain;
+
+public class ReinforcingBar {
+    private final int diameter;
+
+    public ReinforcingBar(int diameter) {
+        this.diameter = diameter;
+    }
+
+    public int getDiameter() {
+        return diameter;
+    }
+}
