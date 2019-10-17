fc815e3135ca8f83377d23efc6aea40aa7dd3417
matjanko <mateusz.jankowski@prokonto.pl>
2019-10-17 20:43:49 +0200

    Add concrete strength class

---
+++ src/main/java/com/github/matjanko/engineer/concrete/cover/domain/concrete/StrengthClass.java
@@ -0,0 +1,13 @@
+package com.github.matjanko.engineer.concrete.cover.domain.concrete;
+
+public enum StrengthClass {
+    C12_15,
+    C16_20,
+    C20_25,
+    C25_30,
+    C30_37,
+    C35_45,
+    C40_50,
+    C45_55,
+    C50_60
+}
--- src/main/java/com/github/matjanko/engineer/concrete/cover/domain/exposure/ExposureClass.java
+++ src/main/java/com/github/matjanko/engineer/concrete/cover/domain/exposure/ExposureClass.java
@@ -1,4 +1,4 @@
-package com.github.matjanko.engineer.concrete.cover.domain.concrete;
+package com.github.matjanko.engineer.concrete.cover.domain.exposure;
 
 public enum ExposureClass {
     XC0, XC1, XC2, XC3, XC4, XD1, XD2, XD3, XS1, XS2, XS3, XF1, XF2, XF3, XF4, XA1, XA2, XA3
