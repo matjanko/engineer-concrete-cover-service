ec972bb98e65df7d2c2a76bf67e381a3a48e61d2
matjanko <mateusz.jankowski@prokonto.pl>
2019-10-17 23:08:16 +0200

    Reinforcing bar test - quick refactor

--- src/test/java/com/github/matjanko/engineer/concrete/cover/domain/bar/ReinforcingBarTest.java
+++ src/test/java/com/github/matjanko/engineer/concrete/cover/domain/bar/ReinforcingBarTest.java
@@ -6,30 +6,24 @@ package com.github.matjanko.engineer.concrete.cover.domain.bar;
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(JUnitParamsRunner.class)
 public class ReinforcingBarTest {
 
-    private ReinforcingBarFactory reinforcingBarFactory;
-
-    @Before
-    public void before() {
-        reinforcingBarFactory = new ReinforcingBarFactory();
-    }
+    private ReinforcingBarFactory reinforcingBarFactory = new ReinforcingBarFactory();
 
     @Test
     @Parameters({ "6", "8", "10", "12", "14", "16", "18", "20", "22", "25", "28", "32", "40" })
-    public void createReinforcingBarCorrectDiameterTest(int diameter) {
-        createReinforcingBarTest(diameter);
+    public void createReinforcingBarCorrectDiameterTest(int input) {
+        createReinforcingBarTest(input);
     }
 
     @Test(expected = IllegalArgumentException.class)
     @Parameters({ "2", "1", "0", "-2", "23", "47"})
-    public void createReinforcingBarInvalidDiameterTest(int diameter) {
-        createReinforcingBarTest(diameter);
+    public void createReinforcingBarInvalidDiameterTest(int input) {
+        createReinforcingBarTest(input);
     }
 
     private void createReinforcingBarTest(int diameter) {
