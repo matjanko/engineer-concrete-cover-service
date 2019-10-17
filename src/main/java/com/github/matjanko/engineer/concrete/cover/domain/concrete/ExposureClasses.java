3e915ddb3fe0164c1b21c364af00e4f636250c03
matjanko <mateusz.jankowski@prokonto.pl>
2019-10-17 20:38:27 +0200

    Add concrete exposure classes

---
+++ src/main/java/com/github/matjanko/engineer/concrete/cover/domain/concrete/ExposureClasses.java
@@ -0,0 +1,5 @@
+package com.github.matjanko.engineer.concrete.cover.domain.concrete;
+
+public enum ExposureClasses {
+    XC0, XC1, XC2, XC3, XC4, XD1, XD2, XD3, XS1, XS2, XS3, XF1, XF2, XF3, XF4, XA1, XA2, XA3
+}
