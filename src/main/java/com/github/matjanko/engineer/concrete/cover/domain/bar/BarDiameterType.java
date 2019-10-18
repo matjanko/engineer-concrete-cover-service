ddbe03ccb74c28fbf3595dc8ee78b02f7ac892b7
matjanko <mateusz.jankowski@prokonto.pl>
2019-10-18 18:31:58 +0200

    Refactor bar diameter type - remove constructor and value field

--- src/main/java/com/github/matjanko/engineer/concrete/cover/domain/bar/BarDiameterType.java
+++ src/main/java/com/github/matjanko/engineer/concrete/cover/domain/bar/BarDiameterType.java
@@ -6,33 +6,15 @@ package com.github.matjanko.engineer.concrete.cover.domain.bar;
 import java.util.stream.Stream;
 
 public enum BarDiameterType {
-    FI_6(6),
-    FI_8(8),
-    FI_10(10),
-    FI_12(12),
-    FI_14(14),
-    FI_16(16),
-    FI_18(18),
-    FI_20(20),
-    FI_22(22),
-    FI_25(25),
-    FI_28(28),
-    FI_32(32),
-    FI_40(40);
+    FI_6, FI_8, FI_10, FI_12, FI_14, FI_16, FI_18, FI_20, FI_22, FI_25, FI_28, FI_32, FI_40;
 
-    private final int value;
-
-    BarDiameterType(int value) {
-        this.value = value;
-    }
-
-    public int getValue() {
-        return value;
+    public int getValue() { ;
+        return Integer.parseInt(this.name().substring(3));
     }
 
     public static BarDiameterType getType(int diameter) {
         return Stream.of(BarDiameterType.values())
-                .filter(d -> d.value == diameter)
+                .filter(d -> d.getValue() == diameter)
                 .findAny()
                 .orElseThrow(() -> new IllegalArgumentException(
                         String.format("Invalid bar diameter %s.", diameter)));
