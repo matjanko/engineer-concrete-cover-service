69c67f297e85698856b705d62906a3fecc994d4a
matjanko <mateusz.jankowski@prokonto.pl>
2019-10-18 20:37:32 +0200

    Refactor concrete strength class

--- src/main/java/com/github/matjanko/engineer/concrete/cover/domain/concrete/StrengthClass.java
+++ src/main/java/com/github/matjanko/engineer/concrete/cover/domain/concrete/StrengthClass.java
@@ -3,29 +3,15 @@ package com.github.matjanko.engineer.concrete.cover.domain.concrete;
 import java.util.stream.Stream;
 
 public enum StrengthClass {
-    C12_15("C12/15"),
-    C16_20("C16/20"),
-    C20_25("C20/25"),
-    C25_30("C25/30"),
-    C30_37("C30/37"),
-    C35_45("C35/45"),
-    C40_50("C40/50"),
-    C45_55("C45/55"),
-    C50_60("C50/60");
-
-    private final String name;
-
-    StrengthClass(String name) {
-        this.name = name;
-    }
+    C12_15, C16_20, C20_25, C25_30, C30_37, C35_45, C40_50, C45_55, C50_60;
 
     public String getName() {
-        return name;
+        return this.name().replace("_", "/");
     }
 
     public static StrengthClass getStrengthClass(String name) {
         return Stream.of(StrengthClass.values())
-                .filter(s -> s.name.equals(name))
+                .filter(s -> s.getName().equals(name))
                 .findAny()
                 .orElseThrow(() -> new IllegalArgumentException(
                         String.format("Invalid concrete strength class %s.", name)));
